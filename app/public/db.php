facd734756886d1d04492bd8d26cbf435e20128a
mao <mao@tigermedia.eu>
2022-03-24 14:56:42 +0100

    updated project to import all json rows.

--- app/public/db.php
+++ app/public/db.php
@@ -12,4 +12,36 @@ try {
     $conn = null;
 }
 
+//Create the table
+// SQL statement for creating new tables
+$createMovies = (
+    "CREATE TABLE IF NOT EXISTS movies( 
+        id INT AUTO_INCREMENT,
+        title VARCHAR(255) NOT NULL,
+        year VARCHAR(255) NOT NULL,
+        rated VARCHAR(255) NOT NULL,
+        released VARCHAR(255) NOT NULL,
+        runtime VARCHAR(255) NOT NULL,
+        genre VARCHAR(255) NOT NULL,
+        director VARCHAR(255) NOT NULL,
+        writer VARCHAR(255) NOT NULL,
+        actors VARCHAR(255) NOT NULL,
+        plot VARCHAR(255) NOT NULL,
+        language VARCHAR(255) NOT NULL,
+        country VARCHAR(255) NOT NULL,
+        awards VARCHAR(255) NOT NULL,
+        poster VARCHAR(255) NULL,
+        metaScore VARCHAR(255) NULL,
+        imdbRating VARCHAR(255) NULL,
+        imdbVotes VARCHAR(255) NULL,
+        imdbID VARCHAR(255) NULL,
+        type VARCHAR(255) NULL,
+        response VARCHAR(255) NULL,
+        image VARCHAR(600) NULL,
+        PRIMARY KEY(id)
+    );"
+);
+
+$conn->exec($createMovies);
+
 ?>
--- app/public/index.php
+++ app/public/index.php
@@ -9,8 +9,6 @@ $jsondata = file_get_contents('./import_files/Film.JSON');
 //convert json object to php associative array
 $data = json_decode($jsondata, true);
 
-//var_dump($data);
-
 foreach($data as $row) {
     //map the movie fields
     $title      = $row['Title'];
@@ -33,52 +31,17 @@ foreach($data as $row) {
     $imdbID     = $row['imdbID'];
     $type       = $row['Type'];
     $response   = $row['Response'];
-    $images     = $row['Images'];
+    $image      = $row['Image'];
+
+    // Saving movies to db part:
+    $insertStmt = $conn->prepare("INSERT INTO   movies (title, year, rated, released, runtime, genre, 
+    director, writer, actors, plot, language, country, awards, 
+    poster, metaScore, imdbRating, imdbVotes, imdbID, type, response, image)
+    VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+    $insertStmt->execute([$title, $year, $rated, $released, $runtime, $genre, $director, $writer, $actors, $plot, $language, 
+    $country, $awards, $poster, $metaScore, $imdbRating, $imdbVotes, $imdbID, $type, $response, $image]);
 }
 
-// Saving to db part:
-
-//Create the table
-// SQL statement for creating new tables
-$statements = [
-    'CREATE TABLE IF NOT EXISTS movies( 
-        id INT AUTO_INCREMENT,
-        title VARCHAR(100) NOT NULL,
-        year int(4) NOT NULL,
-        rated VARCHAR(100) NOT NULL,
-        released VARCHAR(100) NOT NULL,
-        runtime VARCHAR(100) NOT NULL,
-        genre VARCHAR(100) NOT NULL,
-        director VARCHAR(100) NOT NULL,
-        writer VARCHAR(100) NOT NULL,
-        actors VARCHAR(100) NOT NULL,
-        plot VARCHAR(255) NOT NULL,
-        language VARCHAR(100) NOT NULL,
-        country VARCHAR(100) NOT NULL,
-        awards VARCHAR(100) NOT NULL,
-        poster VARCHAR(255) NULL,
-        metaScore VARCHAR(100) NULL,
-        imdbRating VARCHAR(100) NULL,
-        imdbVotes VARCHAR(100) NULL,
-        imdbID VARCHAR(100) NULL,
-        type VARCHAR(100) NULL,
-        response VARCHAR(100) NULL,
-        images VARCHAR(600) NULL,
-        PRIMARY KEY(id)
-    );',
-];
-
-// execute SQL statements
-foreach ($statements as $statement) {
-	$conn->exec($statement);
-}
-
-//insert into mysql table "movies"
-$sql = "INSERT INTO movies(title, year, rated, released, runtime, genre, director, writer, actors, plot, language, 
-country, awards, poster, metaScore, imdbRating, imdbVotes, imdbID, type, response, images)
-VALUES('$title', '$year', '$rated', '$released', '$runtime', '$genre', '$director', '$writer', '$actors', '$plot', '$language', 
-'$country', '$awards', '$poster', '$metaScore', '$imdbRating', '$imdbVotes', '$imdbID', '$type', '$response', '$images')";
-if(!mysql_query($sql,$conn))
-{
-    die('Error : ' . mysql_error());
-}
+// Lukker forbindelsen efter indsættelse.
+$insertStmt = null;
